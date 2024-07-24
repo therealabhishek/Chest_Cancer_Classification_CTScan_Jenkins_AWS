dbea8593ea7e523076e29166ecf4b64f591d7cd7
Abhishek Kulkarni <kul.abhishake@gmail.com>
2024-07-24 09:00:22 +0530

    model trainer completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.CancerClassifier import logger
 from src.CancerClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.CancerClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.CancerClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -27,3 +28,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Training"
+
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
